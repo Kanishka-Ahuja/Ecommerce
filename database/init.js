aabf4bb8397f81ed35e57e73d3bc3ca1994f6b99
Kanishka-Ahuja <kanishkahuja.242@gmail.com>
2023-12-03 21:09:17 +0530

    Applied error handling

--- database/init.js
+++ database/init.js
@@ -1,6 +1,18 @@
 const mongoose = require('mongoose')
+require('dotenv').config();
 
-module.exports = async function init(){
-    await mongoose.connect('mongodb://127.0.0.1:27017/E_Commerce')
-    console.log("Connected to db")
+module.exports = async function init() {
+
+  try {
+
+    await mongoose.connect(process.env.DATABASE_PATH);
+    console.log("Database Connected Successfully");
+
   }
+  catch (error) {
+
+    console.log("Error occured while connecting database :\n", error.message);
+
+  }
+
+}
