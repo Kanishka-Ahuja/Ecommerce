d630fd8a9eae89d618fbbf45b201c0aec79315b1
Kanishka-Ahuja <kanishkahuja.242@gmail.com>
2023-12-12 20:30:54 +0530

    Updated path

---
+++ routes/product.js
@@ -0,0 +1,13 @@
+const express = require('express')
+const router = express()
+
+const {
+    getProducts,
+    showProducts,
+    loadmoreProducts } = require('../controllers/product/get');
+
+router.route('/getproducts').get(getProducts);
+router.route('/home').get(showProducts);
+router.route('/loadmore').get(loadmoreProducts);
+
+module.exports = router;
