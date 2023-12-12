51f37d5cb9d6443527d346bed9240257495d9e49
Kanishka-Ahuja <kanishkahuja.242@gmail.com>
2023-12-12 20:32:12 +0530

    Moved controllers and routers out

--- services/controllers/admincontrollers/getroutes.js
+++
@@ -1,6 +0,0 @@
-const path = require('path')
-const adminPage = async(req,res)=>{
-    res.sendFile(path.join(__dirname+'../../../../admin/admin.html'))
-}
-
-module.exports = {adminPage};
--- services/routes/product.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-const router = express()
-
-const {showProducts,loadmoreProducts} = require('../controllers/productcontrollers/getroutes.js');
-
-router.route('/home').get(showProducts);
-router.route('/loadmore').get(loadmoreProducts);
-
-module.exports = router;
