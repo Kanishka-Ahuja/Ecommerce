94496c14a3aecdfffe2d826b2cae13f6ebd3a5e1
Kanishka-Ahuja <kanishkahuja.242@gmail.com>
2023-12-14 21:09:02 +0530

    Applied error handling using handleError function

--- controllers/admin/get.js
+++ controllers/admin/get.js
@@ -1,28 +1,19 @@
 const path = require('path')
 const fs = require('fs').promises;
+const handleError = require('../error')
 
 const adminPage = async (req, res) => {
 
     try {
 
-        const filepath = path.join(process.cwd(), '/public/admins/admin.html')
-        await fs.access(filepath);
+        const filepath = path.join(process.cwd(), '/public/admin/admin.html')
+        await fs.access(filepath)
         res.sendFile(filepath)
 
     }
-    catch (error) {
+    catch (err) {
 
-        console.log("Error : ", error.message)
-
-        if (error.code === 'ENOENT') {
-            res.redirect('/notfounderror')
-        }
-        else if (error.code === 'EACCES') {
-            res.redirect('/accessdenied')
-        }
-        else {
-            res.redirect('/servererror')
-        }
+        handleError(err,res)
 
     }
 
